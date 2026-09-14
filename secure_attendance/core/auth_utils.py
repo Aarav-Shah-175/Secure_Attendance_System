81ceb0d5400c7eb93ae3e9372745ceec7c2b5363
Aarav-Shah-175 <shah.aarav.175@gmail.com>
2026-09-14 18:18:25 +0530

    chore: Clean Up

--- secure_attendance/core/auth_utils.py
+++
@@ -1,8 +0,0 @@
-import pyotp# type: ignore
-
-def generate_totp_secret():
-    return pyotp.random_base32()
-
-def verify_totp(secret, code):
-    totp = pyotp.TOTP(secret)
-    return totp.verify(code)
